bb13e30ff951750cf83d0222acb42e0020e1e99c
VoTienTai30 <midnight30122000@gmail.com>
2023-07-08 02:48:39 +0700

    add view list teachers

--- Backend/QuizSystemBackend/QuizSystemWeb/Controllers/UserController.cs
+++ Backend/QuizSystemBackend/QuizSystemWeb/Controllers/UserController.cs
@@ -5,41 +5,42 @@ using System.Text.Json;
 
 namespace QuizSystemWeb.Controllers
 {
-	public class UserController : Controller
-	{
-		private readonly HttpClient client = null;
-		private string UserApiUrl = "https://localhost:7049/api/user";
-		public UserController()
-		{
-			client = new HttpClient();
-			var contentType = new MediaTypeWithQualityHeaderValue("application/json");
-			client.DefaultRequestHeaders.Accept.Add(contentType);
-		}
+    public class UserController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string UserApiUrl = "https://localhost:7049/api/user";
+        public UserController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+        }
 
-		public async Task<IActionResult> Index()
-		{ 
-			try
-			{
-				string? token = Request.Cookies["Token"];
-				if (token == null)
-				{
-					return Unauthorized();
-				}
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-				HttpResponseMessage response = await client.GetAsync(UserApiUrl);
-				string strData = await response.Content.ReadAsStringAsync();
+        public async Task<IActionResult> Teachers()
+        {
+            try
+            {
+                string? token = Request.Cookies["Token"];
+                if (token == null)
+                {
+                    return Unauthorized();
+                }
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await client.GetAsync(UserApiUrl);
+                string strData = await response.Content.ReadAsStringAsync();
 
-				var options = new JsonSerializerOptions
-				{
-					PropertyNameCaseInsensitive = true
-				};
-				List<User>? users = JsonSerializer.Deserialize<List<User>>(strData, options);
-				return View(users);
-			} catch (Exception ex)
-			{
-				return Unauthorized();
-			}
-			
-		}
-	}
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                List<User>? users = JsonSerializer.Deserialize<List<User>>(strData, options);
+                return View(users.Where(u => u.RoleId == 2).ToList());
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized();
+            }
+
+        }
+    }
 }
