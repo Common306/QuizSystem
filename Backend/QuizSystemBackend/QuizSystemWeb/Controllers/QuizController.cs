241fdc79db43de48713376bb8cf96539a088c11d
VoTienTai30 <midnight30122000@gmail.com>
2023-07-08 12:50:36 +0700

    update

---
+++ Backend/QuizSystemBackend/QuizSystemWeb/Controllers/QuizController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizSystemWeb.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace QuizSystemWeb.Controllers
+{
+    public class QuizController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string QuizApiUrl = "https://localhost:7049/api/quiz";
+        public QuizController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                string? token = Request.Cookies["Token"];
+                if (token == null)
+                {
+                    return Unauthorized();
+                }
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage response = await client.GetAsync(QuizApiUrl);
+                string strData = await response.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                List<Quiz>? quizes = JsonSerializer.Deserialize<List<Quiz>>(strData, options);
+                return View(quizes);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized();
+            }
+
+        }
+    }
+}
